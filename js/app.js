bc78d2fcc0540f65826f0fa807ba99a8bc67d802
kimaiga <nelsonkimaiga@aol.com>
2016-02-15 22:04:49 +0300

    minor fixes

--- js/app.js
+++ js/app.js
@@ -8,15 +8,15 @@
 var app = angular.module('calendar', [ ]);
 
 app.controller('MainSchedulerCtrl', function($scope) {
-  $scope.events = [
-    { id:1, text:"Task A-12458",
-      start_date: new Date(2016, 10, 12),
-      end_date: new Date(2013, 10, 16) },
-    { id:2, text:"Task A-83473",
-      start_date: new Date(2013, 10, 22 ),
-      end_date: new Date(2013, 10, 24 ) }
-  ];
+//  $scope.events = [
+//    { id:1, text:"Task A-12458",
+//      start_date: new Date(2013, 10, 12),
+//      end_date: new Date(2013, 10, 16) },
+//    { id:2, text:"Task A-83473",
+//      start_date: new Date(2013, 10, 22 ),
+//      end_date: new Date(2013, 10, 24 ) }
+//  ];
 
-  $scope.scheduler = { date : new Date(2013,10,1) };
+//  $scope.scheduler = { date : new Date(2013,10,1) };
 
 });
